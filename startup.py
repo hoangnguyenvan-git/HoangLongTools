09ac4a48ab8396eda81e305d12a9bad3ab35df44
Nguyen Hoang <hoangnbk99@gmail.com>
2025-12-24 16:44:02 +0700

    Update Fix 2024 2025 version

--- startup.py
+++ startup.py
@@ -11,7 +11,13 @@
 # ===============================================================
 
 # HoangLongTools.extension/startup.py
-import os, sys, clr, traceback
+import os
+import sys
+import clr
+
+import System # type: ignore
+from System import AppDomain # type: ignore
+from System.Reflection import Assembly # type: ignore
 
 ext_dir  = os.path.dirname(__file__)
 core_dir = os.path.join(ext_dir, 'lib', 'NguyenHoangCore')
@@ -19,18 +25,37 @@ core_dir = os.path.join(ext_dir, 'lib', 'NguyenHoangCore')
 if core_dir not in sys.path:
     sys.path.append(core_dir)
 
-for file in os.listdir(core_dir):
-    if file.lower().endswith('.dll'):
-        dll_path = os.path.join(core_dir, file)
-        try:
-            asm_name = os.path.splitext(file)[0]
-            clr.AddReference(asm_name)
-        except IOError:
-            try:
-                clr.AddReferenceToFileAndPath(dll_path)
-            except Exception:
-                traceback.print_exc()
+# Resolve dependencies from core_dir
+def _resolve(sender, args):
+    try:
+        name = args.Name.split(',')[0] + ".dll"
+        cand = os.path.join(core_dir, name)
+        if os.path.exists(cand):
+            return Assembly.LoadFrom(cand)
+    except:
+        pass
+    return None
 
+try:
+    AppDomain.CurrentDomain.AssemblyResolve += _resolve
+except:
+    pass
 
+for fn in os.listdir(core_dir):
+    if not fn.lower().endswith('.dll'):
+        continue
 
+    lname = fn.lower()
+    if lname.startswith('revitapi'):
+        continue
+    if lname.startswith('adwindows'):
+        continue
+    if lname.startswith('uiframework'):
+        continue
 
+    dll_path = os.path.join(core_dir, fn)
+
+    try:
+        clr.AddReferenceToFileAndPath(dll_path)
+    except:
+        pass
